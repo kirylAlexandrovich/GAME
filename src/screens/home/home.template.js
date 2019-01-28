a6ce0b8251e9100289ed375c9f3eb66306ce0f4e
Kiryl Alexandrovich <kiryl.alexandrovich@gmail.com>
2019-01-28 14:07:51 +0300

    Revert "feat: add build project"
    This reverts commit 3b8fe3893eeab30fadd57103af6808b909e9ff8f.

---
+++ src/screens/home/home.template.js
@@ -0,0 +1,12 @@
+`  <header id="header">
+    <nav>
+      <ul class="menu">
+        <li><a href="#">О ИГРЕ</a></li>
+        <li><a href="#">ОПИСАНИЕ</a></li>
+        <li><a href="#">СКРИНШОТЫ</a></li>
+      </ul>
+      <h1>Эльфийский лес</h1>
+    </nav>
+  </header> 
+  <script src="landing.js"></script>
+`
